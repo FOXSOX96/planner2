832ae330936f59a56a9389269b0397785d830042
WonTaeJun <dnjsxonws94@gmail.com>
2025-11-12 15:59:57 +0900

    Refactor : Planner Entity 생성자, 세터 입력

--- src/main/java/com/planner2/domain/planner/entity/Planner.java
+++ src/main/java/com/planner2/domain/planner/entity/Planner.java
@@ -5,6 +5,8 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Entity
 @Table(name = "/planners")
@@ -14,8 +16,24 @@ public class Planner {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
     private String title;
     private String content;
+    private LocalDateTime createdAt;
+    private LocalDateTime modifiedAt;
 
+    public Planner (Long id, String name, String title, String content){
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.content = content;
+    }
+
+    public void updatePlanner (Long id, String name, String title, String content){
+        this.id = id;
+        this.name = name;
+        this.title = title;
+        this.content = content;
+    }
 
 }
