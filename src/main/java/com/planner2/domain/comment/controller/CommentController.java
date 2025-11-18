69bdb7dbcef07abee884251f73499c746ce9e34f
WonTaeJun <dnjsxonws94@gmail.com>
2025-11-18 15:58:06 +0900

    Feat : 댓글생성 기능 추가
    Feat : 테스트용 DataInitializer추가

--- src/main/java/com/planner2/domain/comment/controller/CommentController.java
+++ src/main/java/com/planner2/domain/comment/controller/CommentController.java
@@ -1,11 +1,23 @@
 package com.planner2.domain.comment.controller;
 
+import com.planner2.domain.comment.dto.request.CreateCommentRequest;
+import com.planner2.domain.comment.dto.response.CreateCommentResponse;
 import com.planner2.domain.comment.service.CommentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class CommentController {
     private final CommentService commentService;
+
+    //댓글 생성
+    @PostMapping("/users/{userId}/planners/{plannerId}/comments")
+    public ResponseEntity<CreateCommentResponse> createComment(@PathVariable Long userId, @PathVariable Long plannerId, @RequestBody @Valid CreateCommentRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(commentService.createComment(userId, plannerId, request));
+    }
+
 }
--- src/main/java/com/planner2/domain/comment/service/CommentService.java
+++ src/main/java/com/planner2/domain/comment/service/CommentService.java
@@ -1,11 +1,48 @@
 package com.planner2.domain.comment.service;
 
+import com.planner2.domain.comment.dto.request.CreateCommentRequest;
+import com.planner2.domain.comment.dto.response.CreateCommentResponse;
+import com.planner2.domain.comment.entity.Comment;
 import com.planner2.domain.comment.repository.CommentRepository;
+import com.planner2.domain.planner.entity.Planner;
+import com.planner2.domain.planner.repository.PlannerRepository;
+import com.planner2.domain.user.entity.User;
+import com.planner2.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CommentService {
     private final CommentRepository commentRepository;
+    private final UserRepository userRepository;
+    private final PlannerRepository plannerRepository;
+
+    //region 댓글 생성
+    @Transactional
+    public CreateCommentResponse createComment(Long userId,Long plannerId, CreateCommentRequest request) {
+        User user = userRepository.findById(userId).orElseThrow(
+                () -> new IllegalArgumentException("없는 유저입니다.")
+        );
+        Planner planner = plannerRepository.findById(plannerId).orElseThrow(
+                () -> new IllegalArgumentException("없는 일정입니다.")
+        );
+
+        Comment comment = new Comment(
+                user,
+                planner,
+                request.getContent()
+        );
+        Comment savedComment = commentRepository.save(comment);
+
+        return new CreateCommentResponse (
+                savedComment.getId(),
+                savedComment.getContent(),
+                savedComment.getCreatedAt(),
+                savedComment.getModifiedAt()
+        );
+    }
+    //endregion
+
 }
