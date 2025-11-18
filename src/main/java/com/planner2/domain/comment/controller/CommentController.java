5035347eb1d508fba63c023d935ff7dcf83bd0c2
WonTaeJun <dnjsxonws94@gmail.com>
2025-11-18 16:46:01 +0900

    Feat : 선택일정의 댓글 조회 기능 추가

--- src/main/java/com/planner2/domain/comment/controller/CommentController.java
+++ src/main/java/com/planner2/domain/comment/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.planner2.domain.comment.controller;
 
 import com.planner2.domain.comment.dto.request.CreateCommentRequest;
 import com.planner2.domain.comment.dto.response.CreateCommentResponse;
+import com.planner2.domain.comment.dto.response.GetCommentResponse;
 import com.planner2.domain.comment.service.CommentService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,24 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/planners/{plannerId}")
 public class CommentController {
     private final CommentService commentService;
 
     //댓글 생성
-    @PostMapping("/users/{userId}/planners/{plannerId}/comments")
+    @PostMapping("/users/{userId}/comments")
     public ResponseEntity<CreateCommentResponse> createComment(@PathVariable Long userId, @PathVariable Long plannerId, @RequestBody @Valid CreateCommentRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(commentService.createComment(userId, plannerId, request));
+    }
+
+    //선택일정의 댓글 조회
+    @GetMapping("/comments")
+    public ResponseEntity<List<GetCommentResponse>> getComment(@PathVariable Long plannerId) {
+        return ResponseEntity.status(HttpStatus.OK).body(commentService.getComment(plannerId));
     }
 
 }
--- src/main/java/com/planner2/domain/comment/service/CommentService.java
+++ src/main/java/com/planner2/domain/comment/service/CommentService.java
@@ -2,6 +2,7 @@ package com.planner2.domain.comment.service;
 
 import com.planner2.domain.comment.dto.request.CreateCommentRequest;
 import com.planner2.domain.comment.dto.response.CreateCommentResponse;
+import com.planner2.domain.comment.dto.response.GetCommentResponse;
 import com.planner2.domain.comment.entity.Comment;
 import com.planner2.domain.comment.repository.CommentRepository;
 import com.planner2.domain.planner.entity.Planner;
@@ -12,6 +13,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CommentService {
@@ -21,7 +25,7 @@ public class CommentService {
 
     //region 댓글 생성
     @Transactional
-    public CreateCommentResponse createComment(Long userId,Long plannerId, CreateCommentRequest request) {
+    public CreateCommentResponse createComment(Long userId, Long plannerId, CreateCommentRequest request) {
         User user = userRepository.findById(userId).orElseThrow(
                 () -> new IllegalArgumentException("없는 유저입니다.")
         );
@@ -36,12 +40,32 @@ public class CommentService {
         );
         Comment savedComment = commentRepository.save(comment);
 
-        return new CreateCommentResponse (
+        return new CreateCommentResponse(
                 savedComment.getId(),
                 savedComment.getContent(),
                 savedComment.getCreatedAt(),
                 savedComment.getModifiedAt()
         );
+    }
+    //endregion
+
+    //region 선택일정의 댓글 조회
+    @Transactional(readOnly = true)
+    public List<GetCommentResponse> getComment(Long plannerId) {
+        List<Comment> comments = commentRepository.findCommentByPlanner_Id(plannerId);
+
+        List<GetCommentResponse> dtos = new ArrayList<>();
+
+        for (Comment comment : comments) {
+            GetCommentResponse dto = new GetCommentResponse(
+                    comment.getId(),
+                    comment.getContent(),
+                    comment.getCreatedAt(),
+                    comment.getModifiedAt()
+            );
+            dtos.add(dto);
+        }
+        return dtos;
     }
     //endregion
 
